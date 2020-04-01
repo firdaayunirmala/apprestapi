8201f436735c9ff1b60a69fb933598fb38387dd8
firdaayunirmala <ayunirmalafirda@gmail.com>
2020-04-01 12:45:12 +0700

    berhasil tambah data

--- routes.js
+++ routes.js
@@ -9,6 +9,12 @@ module.exports = function (app) {
     app.route('/tampil')
         .get(jsonku.tampilsemuamahasiswa);
 
-        app.route('/tampil/:id')
-        .get(jsonku.tampilberdasarkanid );
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
+
+    app.route('/tambah/:id')
+        .get(jsonku.tambahMahasiswa);
+
+        app.route('/tambah')
+        .post(jsonku.tambahMahasiswa);
 }
