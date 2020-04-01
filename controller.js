dc279c480968d892d3f7cd25d6f184fff8a44057
firdaayunirmala <ayunirmalafirda@gmail.com>
2020-04-01 11:52:12 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -18,3 +18,18 @@ exports.tampilsemuamahasiswa = function (req, res) {
         }
     });
 };
+
+//Menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function (req,res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+     function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        }
+        else {
+            response.ok(rows, res)
+        }
+    });
+};
+
